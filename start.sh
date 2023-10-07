f97aeba7292e0564ad0a42f5e16fa29b912f2640
░▒▓█SΩΠΔLI✿SΔHΔ█▓▒░ <124885646+OFFCode032@users.noreply.github.com>
2023-10-07 06:51:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/LISA-KOREA/UPLOADER-BOT-V3 /UPLOADER-BOT-V3
+git clone https://github.com/OFFCode032/betaurluploaderx /UPLOADER-BOT-V3
 cd /UPLOADER-BOT-V3
 pip3 install -U -r requirements.txt
-echo "Starting Bot.... Please Wait. Check You Log. Bot Edit by @LISA_FAN_KL"
+echo "Starting Bot.... Please Wait. Check You Log. Bot Edit by @sonali00000
 python3 bot.py
